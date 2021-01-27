16812250ababf2d2925cbd75302e557c57d6018d
jesseyh <jessey.harry.23@gmail.com>
2021-01-27 15:17:49 -0500

    Created User Class User Class -   - set values for name and email   - sanitize the user values   - validates the email and sets the boolean value of validity
    TODO -
      - create database class for insertion of data
      - move User class to it's own file

--- src/user_upload.php
+++ src/user_upload.php
@@ -10,6 +10,53 @@ $longopts  = array (
     "dry_run",          // runs all the parts of program but doesn't insert into db
     "help"              // lists all the options with details
 );
+    
+class User
+{
+    public $fname;
+    public $lname;
+    public $email;
+    public $valid;
+    
+    function __construct($fname, $lname, $email) {
+        $this->fname = $fname;
+        $this->lname = $lname;
+        $this->email = $email;
+        //think about redordering this (don't waste time sanitizing garbage emails)
+        $this->sanitize();
+        $this->valid = $this->validate();
+    }
+    
+    /*
+     *
+     */
+    private function validate() {
+        if (filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+            //email is valid so we will clean the remaining values
+            return true;
+        } else {
+            //email is not valid so we set $valid to false
+            return false;
+        }
+    }
+    
+    /*
+     *
+     */
+    private function sanitize() {
+        // remove non letters and white spaces from names and have only the leading letter capitalized
+        $this->fname = str_replace('/\s+/', '', preg_replace('/\PL/u', '', $this->fname));
+        $this->fname = ucfirst(strtolower($this->fname));
+        
+        $this->lname = str_replace('/\s+/', '', preg_replace('/\PL/u', '', $this->lname));
+        $this->lname = ucfirst(strtolower($this->lname));
+        
+        // remove white spaces from emails and lowercase for all letters
+        $this->email = str_replace('/\s+/', '', $this->email);
+        $this->email = strtolower($this->email);
+        
+    }
+}
 
 $options = getopt($shortopts, $longopts);
 
@@ -24,7 +71,7 @@ if (isset($options['create_table'])) {
     $filename = $options['file'];
     if (($file = fopen($filename, "r")) !== FALSE) {
         while (($data = fgetcsv($file, 0, ",")) !== FALSE) {
-            //chec how many columns aer in this row
+            //check how many columns are in this row
             $num = count($data);
             //ensure there are 3 columns
             if ($num == 3) {
@@ -34,12 +81,15 @@ if (isset($options['create_table'])) {
                 
                 echo $fname . " " . $lname . " " . $email . "\n";
                 
-                //TODO: clean data here
+                $user = new User($fname, $lname, $email);
+
+                echo $user->fname . " " . $user->lname . " " . $user->email . " " . $user->valid . "\n";
                 
                 //TODO: insert data into array
                 
-                //TODO: insert row into table
+                //TODO: insert arrray 'row' into table
             } else {
+                //this row does not contain 3 rows and is likely corrupted
                 //throw away this row
             }
         }
